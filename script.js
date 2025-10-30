cb6438541d62e0fa98cc808996a2d98901fd3dc6
kg2468-hub <erikhenrique299@gmail.com>
2025-10-30 09:42:38 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ function abrirPlayer(filme) {
   filmeDuracao.textContent = filme.duracao || "";
   filmeResolucao.textContent = filme.resolucao || "";
 
-  player.src = `https://drive.google.com/file/d/${filme.id}/embedded`;
+  player.src = `https://drive.google.com/file/d/${filme.id}/preview`;
 
   setTimeout(() => {
     if (!player.contentWindow || !filme.id) {
